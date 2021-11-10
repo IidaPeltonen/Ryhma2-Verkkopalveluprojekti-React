86cc630fecd5dbbeadb1ce05284da3be0c858a61
helmioo <81010386+helmioo@users.noreply.github.com>
2021-11-10 18:09:23 +0200

    hajotettu nav ja footer omiin tiedostoihin, ei vielä poistettu app.js:stä -Helmi

---
+++ src/inc/footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function footer() {
+    return (
+        <div>
+            <footer className="row">
+                <div className="col-md-2 col-10">
+                  <p className="text-center ms-4 ">Yhteystiedot</p>
+                </div>
+                <div className="col-md-2 col-10">
+                  <p className="text-center">Tietoa meistä</p>
+                </div>
+                <div className="col-md-2 col-10">
+                  <p className="text-center">UKK</p>
+                </div>
+                <div className="col-md-2 col-10">
+                  <p className="text-center">Rekisteriseloste</p>
+                </div>
+                <div className="col-md-2 col-10 ">
+                  <p className="text-center">Tilaa uutiskirje</p>
+                </div>
+              </footer>
+        </div>
+    )
+}
