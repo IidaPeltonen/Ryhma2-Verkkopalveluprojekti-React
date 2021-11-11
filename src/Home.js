7a748a2603a6d4ea6d3ae6816a6227e81b79f070
IidaPeltonen <n0peii00@students.oamk.fi>
2021-11-11 17:12:29 +0200

    typo

--- src/Home.js
+++ src/Home.js
@@ -29,7 +29,7 @@ export default function Home() {
     <>
       <ol>
         {kirjat?.map(kirjat => (
-          <li key={kirjat.kirjaid}><img src={piina} /><b>{kirjat.kirjanimi}<br />{kirjat.kirjailija}</b><br />{kirjat.vuosi}<br />Kieli: {kirjat.kieli}<br />Kustantaja: {kirjat.kustantaja}<br />{kirjat.trnimi}<br />{kirjat.kuvaus}<br />Hinta: {kirjat.hinta}<br />Varastossa: {kirjat.saldo} kpl </li>
+          <li key={kirjat.kirjaid}><img src={piina} /><br /><b>{kirjat.kirjanimi}<br />{kirjat.kirjailija}</b><br />{kirjat.vuosi}<br />Kieli: {kirjat.kieli}<br />Kustantaja: {kirjat.kustantaja}<br />{kirjat.trnimi}<br />{kirjat.kuvaus}<br />Hinta: {kirjat.hinta}<br />Varastossa: {kirjat.saldo} kpl </li>
           //<li key={kirjat.asid}>Asiakastunnus: {kirjat.astunnus}<br />Asiakkaan nimi: {kirjat.asnimi}<br />Asiakkaan osoite: {kirjat.asosoite}<br />Postinumero: {kirjat.postinro}<br />Postitoimipakka: {kirjat.postitmp}<br />E-mail: {kirjat.email}</li>
           //<li key={kirjat.tilausid}>Asikas id: {kirjat.asid}<br />Tilauspvm: {kirjat.pvm}<br />Tilauksen tila: {kirjat.tila}</li>
 
