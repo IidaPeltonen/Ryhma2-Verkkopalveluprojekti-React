4e96dac74b5271abb2e03b2d7d7c62e9c20f70de
helmioo <81010386+helmioo@users.noreply.github.com>
2021-11-13 16:28:35 +0200

    rekisteriseloste

--- src/inc/Rekisteri.js
+++ src/inc/Rekisteri.js
@@ -4,6 +4,7 @@ export default function Rekisteri() {
     return (
         <div>
             <h3>Rekisteriseloste</h3>
+
             <h4>Rekisterinpitäjä</h4>
             <p>Kirjakauppa X Oy (y-tunnus 0101011-1)</p>
             <p>Kellosaarentie 12 <br/>
@@ -11,6 +12,7 @@ export default function Rekisteri() {
             Seinäjoki</p>
             <p>Puhelin: 040 1234567</p>
             <p>Sähköposti: info@kirjakauppax.fi</p>
+
             <h4>Määritelmät</h4>
             <p>Rekisteröidyllä tarkoitetaan sitä ihmistä,
                  kenen henkilötietoja Kirjakauppa X henkilörekistereissään 
@@ -19,11 +21,21 @@ export default function Rekisteri() {
             <p>Henkilötiedoilla tarkoitetaan rekisteröityyn liittyviä tietoja,
                  kuten nimi, osoite, sähköposti ja puhelinnumero.
             </p>
-            <p>Asiakkaalla tarkoitetaan tässä yhteydessä niitä kuluttajia
-                 ja niiden yritysten ja muiden yhteisöjen (jäljempänä yhdessä ”yritys”)
-                  edustajia, joiden kanssa Suomalaisella on asiakassuhde.</p>
+            <p>Asiakkaalla tarkoitetaan tässä yhteydessä niitä kuluttajia,
+                 joiden kanssa Kirjakauppa X:llä on asiakassuhde.
+            </p>
+          
             <h4>Mihin tarkoituksiin käytämme henkilötietojasi</h4>
+            <p>Suomalainen käsittelee rekisteröityjen henkilötietoja seuraaviin 
+                tarkoituksiin (yhteen tai useampaan samanaikaisesti): asiakasviestintä,
+                 markkinointi sekä tuotteiden ja palveluiden kehittäminen.
+            </p>
+
             <h4>Kenelle luovutamme tietojasi</h4>
+            <p>Henkilötietojesi suojaaminen ja käsittelyn rajoittaminen
+                 tarpeelliseen on Kirjakauppa X:lle tärkeää. Emme jaa tietojasi kolmansille
+                 osapuolille.
+            </p>
         </div>
     )
 }
