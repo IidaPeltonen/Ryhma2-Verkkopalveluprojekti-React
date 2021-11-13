71de18484615dc69327010e91b677d48ba872dd7
jonim-git <jonimaki1@gmail.com>
2021-11-13 17:38:44 +0200

    logo sivulle, siirtyminen etusivulle logosta

--- src/inc/NavBar.js
+++ src/inc/NavBar.js
@@ -3,68 +3,70 @@ import logo from '../img/logo.png';
 import './styles/NavBar.css';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import {useState,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 const URL = 'http://localhost/kauppa';
 
 export default function NavBar() {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        axios.get(URL + '/tuoteKategoria.php')
-            .then((response)=> {
-                const json = response.data;
-                console.log(response);
-                setCategories(json);
-            }).catch(error => {
-                if (error.response === undefined) {
-                    alert(error);
-                } else {
-                    alert(error.response.data.error);
-                }
-            })
-    }, [])
-    
-    return (
-      
-      <div className="row">
-        <nav id="id" className="navbar navbar-expand-md  ">
-          <div className="container-fluid">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#omaMenu"
-              aria-controls="omaMenu" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="omaMenu">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link className="nav-link active" aria-current="page" to="/">Etusivu</Link >
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/ukk">UKK</Link>
-                </li>
-                <li className="nav-item dropdown" >
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                    data-bs-toggle="dropdown" aria-expanded="false">
-                    Kirjat
-                  </a>
-                  <ul id="alasveto" className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    {categories.map(kirja => (
-                        <li>
-                            <Link>
-                            {kirja.trnimi}
-                            </Link>
-                        </li>
-                    ))}
-                   
-                   { <li>
-                      <hr className="dropdown-divider" />
-                    </li>}
-                  </ul>
-                </li>
-              </ul>
-            </div>
+  useEffect(() => {
+    axios.get(URL + '/tuoteKategoria.php')
+      .then((response) => {
+        const json = response.data;
+        console.log(response);
+        setCategories(json);
+      }).catch(error => {
+        if (error.response === undefined) {
+          alert(error);
+        } else {
+          alert(error.response.data.error);
+        }
+      })
+  }, [])
+
+  return (
+
+    <div className="row">
+      <nav id="id" className="navbar navbar-expand-md  ">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#omaMenu"
+            aria-controls="omaMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="col-2">
+          <Link to="/">
+            <img className="img-fluid logo" src={logo}></img>
+            </Link >
           </div>
-        </nav>
+          <div className="collapse navbar-collapse" id="omaMenu">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/ukk">UKK</Link>
+              </li>
+              <li className="nav-item dropdown" >
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                  data-bs-toggle="dropdown" aria-expanded="false">
+                  Kirjat
+                </a>
+                <ul id="alasveto" className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  {categories.map(kirja => (
+                    <li>
+                      <Link>
+                        {kirja.trnimi}
+                      </Link>
+                    </li>
+                  ))}
+
+                  {<li>
+                    <hr className="dropdown-divider" />
+                  </li>}
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </nav>
+    </div>
+  )
 }
