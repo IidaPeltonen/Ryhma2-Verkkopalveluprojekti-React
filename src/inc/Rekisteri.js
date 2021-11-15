156468d354de7bc212ef9aa33b0c1624b5817142
helmioo <81010386+helmioo@users.noreply.github.com>
2021-11-15 20:31:42 +0200

    alasivujen tyylittelyä alkuun -helmi

--- src/inc/Rekisteri.js
+++ src/inc/Rekisteri.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import '../App.css'
 
 export default function Rekisteri() {
     return (
-        <div>
-            <h3>Rekisteriseloste</h3>
+        <div className="col-md-4 ms-4 mb-4">
+            <h3 className="mb-3">Rekisteriseloste</h3>
 
-            <h4>Rekisterinpitäjä</h4>
+            <h4 className="mb-3">Rekisterinpitäjä</h4>
             <p>Kirjakauppa X Oy (y-tunnus 0101011-1)</p>
             <p>Kellosaarentie 12 <br/>
             60100<br/>
@@ -13,7 +14,7 @@ export default function Rekisteri() {
             <p>Puhelin: 040 1234567</p>
             <p>Sähköposti: info@kirjakauppax.fi</p>
 
-            <h4>Määritelmät</h4>
+            <h4 className="mb-3">Määritelmät</h4>
             <p>Rekisteröidyllä tarkoitetaan sitä ihmistä,
                  kenen henkilötietoja Kirjakauppa X henkilörekistereissään 
                  käsittelee tässä rekisteriselosteessa kuvatussa roolissa.
@@ -25,13 +26,13 @@ export default function Rekisteri() {
                  joiden kanssa Kirjakauppa X:llä on asiakassuhde.
             </p>
           
-            <h4>Mihin tarkoituksiin käytämme henkilötietojasi</h4>
+            <h4 className="mb-3">Mihin tarkoituksiin käytämme henkilötietojasi</h4>
             <p>Suomalainen käsittelee rekisteröityjen henkilötietoja seuraaviin 
                 tarkoituksiin (yhteen tai useampaan samanaikaisesti): asiakasviestintä,
                  markkinointi sekä tuotteiden ja palveluiden kehittäminen.
             </p>
 
-            <h4>Kenelle luovutamme tietojasi</h4>
+            <h4 className="mb-3">Kenelle luovutamme tietojasi</h4>
             <p>Henkilötietojesi suojaaminen ja käsittelyn rajoittaminen
                  tarpeelliseen on Kirjakauppa X:lle tärkeää. Emme jaa tietojasi kolmansille
                  osapuolille.
--- src/inc/Uutiskirje.js
+++ src/inc/Uutiskirje.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../App.css'
 
 export default function Uutiskirje() {
     return (
-        <div>
+        <div className="col-md-3 ms-4 mb-4">
             <h3>Tilaa uutiskirje</h3>
         </div>
     )
