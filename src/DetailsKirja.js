55d98f7457e67a45e7111518218efb6db6550575
IidaPeltonen <n0peii00@students.oamk.fi>
2021-11-16 15:08:18 +0200

    frontin muokkausta kirjan detaljille -iida

--- src/DetailsKirja.js
+++ src/DetailsKirja.js
@@ -1,21 +1,39 @@
 import React from 'react'
-import './App.css'
+import './inc/styles/Details.css';
 import karry from './img/karry.png'
 
 export default function Detail(props) {
-    return (
-        <div id="detail">
-            <img id="detailKuva" src={props.kuva}></img>
-            <h1>{props.kirjanimi}</h1>
-            <h1>{props.kirjailija}</h1>
-            <p>Hinta: {props.hinta}€</p>
-            <p>{props.kuvaus}</p>
-            <p>Julkaisuvuosi: {props.vuosi} </p>
-            <p>Kieli: {props.kieli}</p>
-            <p>Kustantaja: {props.kustantaja}</p>
-            <p>Genre: {props.trnimi}</p>
-            <img id="detailKarry" src={karry}></img><br />
-            <button className="btn-primary"><a href="#" onClick={props.close}>Takaisin listaukseen</a></button>
+    return  (
+        <div id="detail" className="row">
+            <div className="col-5">
+                <img id="detailKuva" src={props.kuva}></img>
+            </div>
+            <div className="col-1">
+
+            </div>
+            <div className="col-6">
+                <h1 id="center">{props.kirjanimi}</h1>
+                <h2 id="center">{props.kirjailija}</h2>
+                <p>{props.kuvaus}</p>
+                <p>Julkaisuvuosi: {props.vuosi} </p>
+                <p>Kieli: {props.kieli}</p>
+                <p>Kustantaja: {props.kustantaja}</p>
+                <p>Genre: {props.trnimi}</p>
+            </div>
+            <div className="col-1">
+
+            </div>
+            <div className="col-3">
+                <button className="btn-primary"><a href="#" onClick={props.close}>Takaisin listaukseen</a></button>
+            </div>
+            <div className="col-3">
+
+            </div>
+            <div className="col-4">
+                <p>Hinta: {props.hinta}€</p>
+                <img id="detailKarry" src={karry}></img><br />
+            </div>
+
         </div>
 
     )
