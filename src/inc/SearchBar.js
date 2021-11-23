8deb0e659be9325b4e8f8067fb9bc372fbad3ee6
J <jonimaki1@gmail.com>
2021-11-24 01:45:42 +0200

    Haku kirjailijan mukaan

--- src/inc/SearchBar.js
+++ src/inc/SearchBar.js
@@ -9,7 +9,7 @@ export default function SearchBar({ placeHolder, kirjat, Detail, addToCart }) {
     const [wordEntered, setWordEntered] = useState('')
     const [valittuKirja, setValittuKirja] = useState(null)
 //  Hakulogiikka-> inputista otetaan wordEntered. wordEntered arvoksi annetaan searchWord.
-//  Jos syötetty searchWord sisältää samoja sanoja/kirjaimia jne. kuin mitä kirjanimiä tietokannassa on, newFilter suodattaa kaikki ne kirjat 
+//  Jos syötetty searchWord sisältää samoja sanoja/kirjaimia jne. kuin mitä kirjanimiä tai kirjailijan nimiä tietokannassa on, newFilter suodattaa kaikki ne kirjat 
 //  joiden nimessä searchWord esiintyy. 
 //  Jos searchWord eli ts. inputissa ei ole mitään, filteredData-taulukko asetetaan tyhjäksi.
 // Tän saa vaihtaa paremmaksi, jos tuntuu siltä että tämä on huono.
@@ -19,7 +19,9 @@ export default function SearchBar({ placeHolder, kirjat, Detail, addToCart }) {
         const searchWord = e.target.value
         setWordEntered(searchWord)
         const newFilter = kirjat.filter((value) => {
-            return value.kirjanimi.toLowerCase().includes(searchWord.toLowerCase());
+            return value.kirjanimi.toLowerCase().includes(searchWord.toLowerCase()) 
+            || 
+            value.kirjailija.toLowerCase().includes(searchWord.toLowerCase())
         })
 
         if (searchWord === "") {
@@ -85,7 +87,7 @@ export default function SearchBar({ placeHolder, kirjat, Detail, addToCart }) {
                 {/* Täällä mapataan filteröity data. slice(0,4) määrittää, että ei voi näkyä kuin 4 hakutulosta */}
                 {/* ** Korjattavaa: Näyttää hakutulokset missä sattuu ja tulokset tulostuu hakukentän (siis inputin) päälle ja hakukenttä pomppii ** */}
                 <li>
-                    {filteredData.length != 0 && (
+                    {filteredData.length !== 0 && (
                         <div className="input-group dataResult mb-3">
                             {filteredData?.slice(0, 4).map((kirja) => (
                                 <div key={kirja.kirjaid}>
