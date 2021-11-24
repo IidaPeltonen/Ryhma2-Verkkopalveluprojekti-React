f1a8e94c28307d28fd9ddaafa81eae8e368e3b8e
IidaPeltonen <n0peii00@students.oamk.fi>
2021-11-24 11:47:56 +0200

    ostoskorin lukema korjattu -iida

--- src/inc/Cart.js
+++ src/inc/Cart.js
@@ -4,6 +4,12 @@ import '../App.css'
 import karry from '../img/karry.png'
 
 export default function Cart ({ cart }) {
+   //lasketaan kaikki ostoskorin itemit etusivun näkymää varten
+   let maara = 0;
+    for (let i = 0; i < cart.length; i++) {
+      //lasketaan taulukon rivi, sieltä tuotteen amount ja lisätään se maaraan
+      maara+=cart[i].amount
+    } 
   return (
     <div>
     <Link to='/order'>
@@ -19,7 +25,7 @@ export default function Cart ({ cart }) {
         <path d='M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z' />
       </svg> */}
       {/* KUINKA TÄHÄN SAADAAN KAIKKIEN ITEMEIDEN MÄÄRÄ */}
-      <span className='no-underline mx-auto ms-2 me-2'>{(cart.length)}</span>
+      <span className='no-underline mx-auto ms-2 me-2'>{(maara)}</span>
     </Link>
     </div>
   )
