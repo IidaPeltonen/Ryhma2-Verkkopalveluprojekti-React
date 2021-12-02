133f6783623c33447d7fb50cc963e59a103abd4a
helmioo <81010386+helmioo@users.noreply.github.com>
2021-12-02 20:21:32 +0200

    css-sekoilua

--- src/Detail.js
+++ src/Detail.js
@@ -6,56 +6,59 @@ import 'react-toastify/dist/ReactToastify.css'
 import { useHistory } from 'react-router-dom'
 
 //yhden kirjan tiedot
-export default function Detail ({ kirja, addToCart }) {
+export default function Detail({ kirja, addToCart }) {
   const history = useHistory()
-  function notify () {
+  function notify() {
     toast('Kirja lisätty ostoskoriin!')
   }
 
   return (
-    <div id='detail' className='row'>
-      <div className='col-5'>
-        <img id='detailKuva' src={kirja.kuva} alt='kirjan kansikuva'></img>
-      </div>
-      <div className='col-1'></div>
-      <div className='col-6'>
-        <h1 className='centerh1'>{kirja.kirjanimi}</h1>
-        <h2 className='centerh2'>{kirja.kirjailija}</h2>
-        <p>{kirja.kuvaus}</p>
-        <p>Julkaisuvuosi: {kirja.vuosi} </p>
-        <p>Kieli: {kirja.kieli}</p>
-        <p>Kustantaja: {kirja.kustantaja}</p>
-      </div>
-      <div className='col-1'></div>
-      <div className='col-3'>
-        <button className='btn-primary p-2 backtoList' onClick={history.goBack}>
-          Takaisin listaukseen
-        </button>
-      </div>
-      <div className='col-3'></div>
-      <div className='col-4'>
-        <p>Hinta: {kirja.hinta}€</p>
-        <button
-          className='btn'
-          type='button'
-          onClick={function (event) {
-            addToCart(kirja)
-            notify()
-          }}
-        >
-          <img id='pieni' src={karry} alt='ostoskärry' />
-        </button>
-        <ToastContainer
-          position='bottom-right'
-          autoClose={4000}
-          hideProgressBar={true}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
+    <div id='detail' className='container'>
+      <div className="row">
+        <div className='col-5'>
+          <img id='detailKuva' src={kirja.kuva} alt='kirjan kansikuva'></img>
+        </div>
+        <div className='col-1'></div>
+        <div className='col-6'>
+          <h1 className='centerh1'>{kirja.kirjanimi}</h1>
+          <h2 className='centerh2'>{kirja.kirjailija}</h2>
+          <p>{kirja.kuvaus}</p>
+          <p>Julkaisuvuosi: {kirja.vuosi} </p>
+          <p>Kieli: {kirja.kieli}</p>
+          <p>Kustantaja: {kirja.kustantaja}</p>
+          <p>Hinta: {kirja.hinta}€</p>
+        </div>
+       
+        <div className="row">
+          <div className="col-9">
+            <button className='btn-primary p-2 backtoList' onClick={history.goBack}>
+              Takaisin listaukseen
+            </button>
+          </div>
+          <div className="col-1">
+            <button
+              className='btn'
+              type='button'
+              onClick={function (event) {
+                addToCart(kirja)
+                notify()
+              }}
+            >
+             Lisää ostoskoriin
+            </button>
+          </div>
+          <ToastContainer
+            position='bottom-right'
+            autoClose={4000}
+            hideProgressBar={true}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </div>
       </div>
     </div>
   )
