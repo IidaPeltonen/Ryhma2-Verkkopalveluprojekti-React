a9cfdd25fdd188ee35e6966ad04a72a684230956
IidaPeltonen <n0peii00@students.oamk.fi>
2021-12-12 17:59:44 +0200

    tilauksen poisto

--- src/Tilaus.js
+++ src/Tilaus.js
@@ -1,6 +1,3 @@
-
-// EDITIN NÄYTTÖKENTTIEN TEKO KESKEN, SAA JATKAA
-
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
@@ -9,22 +6,16 @@ import './inc/styles/Admin.css'
 function Tilaus ({ url }) {
   const [tilaukset, setTilaukset] = useState([])
   const [tilaus, setTilaus] = useState('')
-  const [editTilaus, setEditTilaus] = useState(null)
+
   //tilauksen muuttujat
   const [tilausnumero, setTilausnumero] = useState('')
   const [asid, setAsid] = useState('')
-  const [pvm, setPvm] = useState('') //tarvitaanko, jos tulee automaattina?
   const [tila, setTila] = useState('')
-  const [editTilausnro, setEditTilausnro] = useState('') 
-  const [editAsid, setEditAsid] = useState('')
-  const [editPvm, setEditPvm] = useState('') //tarvitaanko, jos tulee automaattina?
-  const [editTila, setEditTila] = useState('')
+
   //tilausrivin muuttujat
   const [kirjaid, setKirjaid] = useState('') 
   const [kpl, setKpl] = useState('')
-  const [editTilausnro2, setEditTilausnro2] = useState('') //tarvitaanko, jos ei saa editoida?
-  const [editKirjaid, setEditKirjaid] = useState('') //tarvitaanko, jos ei saa editoida?
-  const [editKpl, setEditKpl] = useState('')
+
 
   useEffect(() => {
     axios
@@ -55,39 +46,23 @@ function Tilaus ({ url }) {
       })
   } 
 
-//olemassaolevan tilausrivin päivitys
-function setEditedTilaus(tilaus) {
-  setEditTilaus(tilaus)
-  setEditKirjaid(kirjaid)
-  setEditKpl(kpl)
-}
-
-function paivitaRivi(e) {
-  e.preventDefault()
-  const json = JSON.stringify({
-    tilausnumero: tilausnumero, //ei muuteta
-    kirjaid: kirjaid, //ei muuteta
-    kpl: editKpl
-  })
-  axios
-    .post(url + 'updateTilausrivi.php', json, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-      <br></br>
-      tilaukset[
-        tilaukset.findIndex(tilaus => tilaus.tilausnro === editTilaus.tilausnro && tilaus.kirjaid === editTilaus.kirjaid) // eimuuteta
-      ].kpl = editKpl
-     
-      setTilaukset([...tilaukset])
-      setEditedTilaus(null)
-    })
-    .catch(error => {
-      alert(error.response ? error.response.data.error : error)
-    })
-}
+  //olemassaolevan tilauksen poisto
+  function removeTilaus(tilausnumero) {
+    const json = JSON.stringify({ tilausnro: tilausnumero })
+    axios
+      .post(url + 'deleteTilaus.php', json, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => {
+        const newListWithoutRemoved = tilaukset.filter(tilaus => tilaus.tilausnro !== tilausnumero)
+        setTilaukset(newListWithoutRemoved)
+      })
+      .catch(error => {
+        alert(error.response ? error.response.data.error : error)
+      })
+  } 
 
   let unuunun = 0;
 
@@ -104,7 +79,10 @@ function paivitaRivi(e) {
               <p><b>Tilauksen tila:</b> {tilaus.tila}</p>
               <p><b>Tilausaika: </b>{tilaus.pvm} </p>
               <p><b>Asiakkaan tunnus:</b> {tilaus.astunnus} <b>
-                Asiakas: </b>{tilaus.asetunimi} {tilaus.assukunimi}</p> 
+                Asiakas: </b>{tilaus.asetunimi} {tilaus.assukunimi}
+                <button className='delete' onClick={() => removeTilaus(tilaus.tilausnro)}>
+                    Poista tilaus
+                </button>  </p> 
               <hr />
               <p>{tilaus.kirjanimi} 
               {tilaus.kpl} kpl  
