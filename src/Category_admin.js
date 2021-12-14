9a2dcc8589739beabbe1f3e8a91f48412e0bb3fc
IidaPeltonen <n0peii00@students.oamk.fi>
2021-12-14 19:02:42 +0200

    siivousta

--- src/Category_admin.js
+++ src/Category_admin.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 import './inc/styles/Admin.css'
-import { ToastContainer, toast } from 'react-toastify'
+import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 function notifyAdd() {
